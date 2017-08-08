ef0749bfca36a84343f2f6827353bb592e4f2980
crebeck <cbecker91@gmx.de>
2017-08-08 13:12:50 +0200

    version with tests, 08.08.17

--- survey/tests.py
+++ survey/tests.py
@@ -8,6 +8,32 @@ from .models import Constants
 class PlayerBot(Bot):
 
     def play_round(self):
-        yield (views.Demographics, {'field_of_study' = })
-        yield SubmissionMustFail(views.Demographics, {'age': -5 })
-        yield (views.Bmi)
+        yield (views.Instructions)
+
+        for test_number in [-1, -2, 4, 0.5]:
+            yield SubmissionMustFail(views.Demographics, {
+                'age': 25,
+                'field_of_study': "",
+                'female': 0,
+                'height': test_number,
+                'weight': 100,
+                'likes_experiment': "Yes, of course!"
+                })
+
+
+# class PlayerBot(Bot):
+#
+#     def play_round(self):
+#         yield SubmissionMustFail(views.Demographics, {
+#             'age': 25,
+#             'field_of_study': "",
+#             'female': 3,
+#             'height': 1.5,
+#             'weight': 100,
+#             'likes_experiment': "Yes, of course!"
+#         })
+#
+#         assert self.player.bmi == 100
+#         assert self.player.bmi_eval == "You are overweight, you fat fuck!"
+#
+#         yield (views.Bmi)
